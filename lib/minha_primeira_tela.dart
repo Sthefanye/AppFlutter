3be803488cc11bb9e035e934eae311bed3891a5d
Sthefanye <sthefanye_olive@outlook.com>
2023-03-23 11:54:16 -0400

    Initial commit

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,60 @@
+/*
+Container(
+color: Colors.white,
+child: Column(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+crossAxisAlignment: CrossAxisAlignment.center,
+children: [
+Stack(
+alignment: AlignmentDirectional.center,
+children: [
+Container(color: Colors.blue, width: 100, height: 100),
+Container(color: Colors.orange, width: 50, height: 50),
+],
+),
+Stack(
+alignment: AlignmentDirectional.center,
+children: [
+Container(color: Colors.orange, width: 100, height: 100),
+Container(color: Colors.blue, width: 50, height: 50),
+],
+),
+Row(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+crossAxisAlignment: CrossAxisAlignment.center,
+children: [
+Container(
+color: Colors.cyan,
+width: 50,
+height: 50,
+),
+Container(
+color: Colors.pinkAccent,
+width: 50,
+height: 50,
+),
+Container(
+color: Colors.purple,
+width: 50,
+height: 50,
+)
+],
+),
+Container(
+color: Colors.amber,
+width: 30000,
+height: 30,
+child: Text(
+'Diamante amarelo',
+style: TextStyle(color: Colors.black, fontSize: 28),
+textAlign: TextAlign.center,
+),
+),
+ElevatedButton(
+onPressed: () {
+print('Você apertou o botão.');
+},
+child: Text('Aperte o botão!'))
+],
+),
+)*/
